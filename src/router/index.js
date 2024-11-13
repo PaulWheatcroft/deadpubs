c851136d4da5ca0190775a2d47d078a074dde235
Paul Wheatcroft <72826129+PaulWheatcroft@users.noreply.github.com>
2024-11-13 13:40:32 +0000

    fix: change routing to history: createWebHashHistory(), becuase of github pages support

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import AddEstablishment from '../pages/AddEstablishment.vue'
 import EditEstablishment from '../pages/EditEstablishment.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
